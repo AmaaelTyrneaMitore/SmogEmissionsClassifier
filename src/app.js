c3f2fee994e01a1428f0b24f91be789b0423901c
Shashank Singh <96770927+AmaaelTyrneaMitore@users.noreply.github.com>
2023-11-19 22:32:42 +0530

    🌟 Update: Plot chost history & fix README
    - Add node-remote-plot as project dependency
    - Plot cost history for visualization
    - Fix README.md to ignore the new graph file
    - Refactored tokens and comments more meaninful & descriptive ones

--- src/app.js
+++ src/app.js
@@ -1,40 +1,52 @@
 import { join } from 'path';
 import appRootPath from 'app-root-path';
+import plot from 'node-remote-plot';
 
 import loadCSV from './utils/csv-loader.js';
 import LogisticRegression from './models/logistic-regression.js';
 
-// Load the CSV file path
+// File path for CSV data
 const csvFilePath = join(appRootPath.path, 'data', 'vintage_cars_data.csv');
 
 // Define options for loading CSV data
 const csvLoadingOptions = {
-  // Columns to be considered as data and labels
+  // Define columns for data and labels
   dataColumns: ['horsepower', 'displacement', 'weight'],
   labelColumns: ['passedemissions'],
 
-  // Options for data processing
+  // Configure data processing
   shuffle: true,
   splitTest: 50,
 
-  // Define converters to encode 'passedemissions' as 1 or 0
+  // Convert 'passedemissions' to binary (1 or 0)
   converters: {
     passedemissions: (ifPassed) => (ifPassed === 'TRUE' ? 1 : 0),
   },
 };
 
-// Load the CSV data using the provided options
+// Load CSV data based on the provided options
 const { features, labels, testFeatures, testLabels } = loadCSV(csvFilePath, csvLoadingOptions);
 
-const regression = new LogisticRegression(features, labels, {
+// Initialize and train Logistic Regression model
+const logisticRegressionModel = new LogisticRegression(features, labels, {
   learningRate: 0.5,
   iterations: 100,
   batchSize: 50,
   decisionBoundary: 0.5,
 });
 
-regression.train();
+logisticRegressionModel.train();
 
-const modelAccuracy = regression.test(testFeatures, testLabels);
+// Test the trained model and calculate accuracy
+const modelAccuracy = logisticRegressionModel.test(testFeatures, testLabels);
 
 console.log(`\n\n[+] Model Accuracy: ${modelAccuracy}%\n\n`);
+
+// Plot cost history for visualization
+plot({
+  x: logisticRegressionModel.costHistory.reverse(),
+  name: 'data/cost_history',
+  title: 'Cost History',
+  xLabel: 'No of Iterations #',
+  yLabel: 'Cost',
+});
